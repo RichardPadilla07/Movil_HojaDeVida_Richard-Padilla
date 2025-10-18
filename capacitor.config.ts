ac60dec99458acdd1976adf52a2781274e725713
RichardPadilla07 <richard.padilla@epn.edu.ec>
2025-10-18 13:07:38 -0500

    consulta 1

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,25 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'HojaDeVida',
-  webDir: 'www'
+  appId: 'com.richardpadilla.hojadevida',
+  appName: 'Hoja de Vida',
+  webDir: 'www',
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 3000,
+      launchAutoHide: true,
+      launchFadeOutDuration: 500,
+      backgroundColor: "#3880ff",
+      androidSplashResourceName: "splash",
+      androidScaleType: "CENTER_CROP",
+      showSpinner: false,
+      androidSpinnerStyle: "large",
+      iosSpinnerStyle: "small",
+      spinnerColor: "#ffffff",
+      splashFullScreen: true,
+      splashImmersive: true
+    }
+  }
 };
 
 export default config;
